002ca3729da9539bc24da9f63209f7247cfb9559
Ebelechukwu Doris Okolo <dorisebele47@gmail.com>
2023-06-24 10:21:39 +0100

    initial commit

---
+++ src/main/java/com/wallet/shieldpay/ShieldPayApplication.java
@@ -0,0 +1,13 @@
+package com.wallet.shieldpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShieldPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShieldPayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wallet/shieldpay/ShieldPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallet.shieldpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShieldPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
