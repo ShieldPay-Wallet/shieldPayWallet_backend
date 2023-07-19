35633ac27a910030f074c877c907739588ea340e
LordNedStark <osisiogubenjamin1@gmail.com>
2023-07-19 13:10:15 +0100

    first actual commit

--- src/main/java/com/wallet/shieldpay/services/serviceImplementations/MailSenderService.java
+++ src/main/java/com/wallet/shieldpay/services/serviceImplementations/MailSenderService.java
@@ -1,11 +1,14 @@
 package com.wallet.shieldpay.services.serviceImplementations;
 
+import com.wallet.shieldpay.MockitoCheck.Example;
 import com.wallet.shieldpay.models.UtilityModels.EmailCreator;
 import com.wallet.shieldpay.services.serviceInterface.MailSenderServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Method;
 
 @Service
 public class MailSenderService implements MailSenderServiceInterface {
@@ -19,11 +22,12 @@ public class MailSenderService implements MailSenderServiceInterface {
         message.setTo(emailCreator.getReceiverEmail());
         message.setSubject(emailCreator.getSubject());
 
-//        javaMailSender.send(message);
+        javaMailSender.send(message);
     }
 //    public void sendMimeMail(EmailCreator emailCreator){
 //        MimeMessage mimeMessage = mailSender.createMimeMessage(emailCreator);
 //        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage,true);
 //
 //    }
+
 }
